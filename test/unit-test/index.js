f4e39ef9b82b604c5270c495c14213bfc7810364
Casey Cross <hellocaseycross@gmail.com>
2016-08-16 16:28:33 -0600

    Got servers up and running.

---
+++ test/unit-test/index.js
@@ -0,0 +1,45 @@
+const assert = require('chai').assert;
+
+// var $ = require('jQuery');
+
+
+
+describe('our test bundle', function () {
+  it('should work', function () {
+    assert(true);
+  });
+});
+
+//The application should have a section for displaying chat messages.
+
+describe('display chat messages', function() {
+  it('should have a section for displaying chat messages', function() {
+    // $('#header').should.have.class('foo');
+    // expect($('body')).to.have.class('foo');
+
+    // $('section').should.have.class('chat-box');
+    // expect($('section')).to.have.class('chat-box');
+
+    // var newSection = getElementsByTagName('section');
+    // assert(newSection.attr('class') === 'chat-box', 'section has class chat-box');
+  });
+});
+
+
+
+describe('reverse chronological order', function() {
+  it('should display messages in reverse chronological order', function() {
+    var ideaOne = Date.now();
+    var ideaTwo;
+    function delayIdeaTwo(ideaTwo) {
+      ideaTwo = window.setTimeout(returnIdeaTwo, 2000);
+      return ideaTwo;
+    }
+    function returnIdeaTwo (ideaTwo) {
+      ideaTwo = Date.now();
+      return ideaTwo;
+    }
+    assert.isAbove(ideaTwo, ideaOne, 'ideaTwo is greater than ideaOne');
+
+  });
+});
